2dde5f70173aa5baab79b8885bf3f861db8b4de0
Brunoskk <themaximusgames@gmail.com>
2019-06-27 07:31:18 -0300

    Initial commit

---
+++ PHP/ValidaLogin.php
@@ -0,0 +1,30 @@
+<?php
+    session_start();
+    $pdo = new PDO('mysql:host=localhost;dbname=foodcontroler;charset=utf8', "root", "");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $consulta = $pdo->prepare("SELECT * FROM usuario where login = ? and senha = ?");
+    $consulta->bindParam(1, $_POST['login'], PDO::PARAM_STR);
+    $consulta->bindParam(2, $_POST['senha'], PDO::PARAM_STR);
+
+    $consulta->execute();
+    $linha = $consulta->fetch(PDO::FETCH_ASSOC); 
+ 
+    $email = $linha['login']; 
+    $senha = $linha['senha']; 
+    
+    $pdo=null;
+
+     if ( $_POST['login'] == $email && $_POST['senha'] == $senha && $email !="" && $senha !="" ){
+     $_SESSION['logado'] = 'SIM';
+     $_SESSION['horarioLogin'] = time();
+     echo "<script>window.location.replace('http://localhost/Food_Controler_26_06/index.php');</script>";
+	 }
+	 
+     else{
+	 echo "<script>alert('Email ou senha incorreto!')</script>";
+     echo "<script>window.location.replace('http://localhost/Food_Controler_26_06/login.php');</script>";
+     header("Cache-Control: no-cache, must-revalidate");
+     header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); 
+    }
+?>     
---
+++ PHP/cadastroUsu.php
@@ -0,0 +1,32 @@
+<?php
+$nome = $_POST['nome'];
+$sobrenome = $_POST['sobrenome'];
+$login = $_POST['login'];
+$senha = $_POST['senha'];
+$TipoUsu = $_POST['TipoUsu'];
+
+$pdo = new PDO('mysql:host=localhost;dbname=foodcontroler;charset=utf8', "root", "");
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$consulta = $pdo->prepare("SELECT * FROM usuario where login = ?");
+$consulta->bindParam(1, $_POST['login'], PDO::PARAM_STR);
+
+$consulta->execute();
+$linha = $consulta->fetch(PDO::FETCH_ASSOC); 
+
+$email = $linha['login'];
+
+if($email ==""){
+$pdo = new PDO('mysql:host=localhost;dbname=foodcontroler;charset=utf8', "root", "");
+$sql = "insert into usuario (nome, sobrenome , login , senha, )  Values(?,?,?,?,?)";
+$query = $pdo->prepare($sql);
+$query->execute(array($nome,$sobrenome,$login,$senha,$TipoUsu));
+$pdo = null;
+
+echo"<script>alert('cadastro realizado!');</script>";
+echo "<script>window.location.replace('http://localhost/Food_Controler_26_06/index.php');</script>";
+}else{
+    echo "<script>alert('Usuario Existente!');</script>";
+    echo "<script>window.location.replace('http://localhost/Food_Controler_26_06/index.php');</script>";
+}
+?>
