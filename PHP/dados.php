22fc3164284685c2c7499cdba81c1653e7fe29a3
Brunoskk <52199541+Brunoskk@users.noreply.github.com>
2019-06-29 18:52:08 -0300

    Atualização de 29/06/19 add: dados.php para busca no banco

--- PHP/dados.php
+++ PHP/dados.php
@@ -1,3 +1,25 @@
 <?php
-//utilizado para puxar as informações do usuario
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=foodcontroler', "root", "");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $pdo->prepare("SELECT nome,sobrenome,tipousu,nomeProd,quantidade,tipoProd,fornecedor FROM usuario INNER JOIN produtos ON idUsu = idProdE");
+    $stmt ->execute();
+
+} catch (PDOException $e) {
+    echo $e;
+}
+
+$teste = array();
+    $i = 0;
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    
+    $teste[$i] = $row['nome']." ".$row['sobrenome']." ".$row['tipousu']." ".$row['nomeProd'];
+    
+    $i++;
+}
+
+echo $teste[0]."<br>";
+echo $teste[1];
+
 ?>
